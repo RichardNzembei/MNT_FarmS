3a19742893b9d17262897ae75cb9ee53559bc5f4
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-15 21:46:41 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,31 +10,12 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       VitePWA({
-        manifest: {
-          name: 'ReubenS_FarmS',
-          short_name: 'ReubenS',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
-          display: 'standalone',
-          icons: [
-            {
-              src: '/favicon/192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-              purpose: 'maskable'
-            },
-            {
-              src: '/favicon/512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable'
-            }
-          ]
-        },
+        srcDir: 'public',
+        filename: 'manifest.json',
         registerType: 'autoUpdate',
         workbox: {
           globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-          swDest: 'public/sw.js',
+          swDest: 'public/sw.js'
         }
       })
     ]
