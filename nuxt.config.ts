025534886d2d58b8b06ddaf3a70455132166a813
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-15 22:00:43 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,34 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       VitePWA({
-        srcDir: 'public',
-        filename: 'manifest.json',
         registerType: 'autoUpdate',
+        manifest: {
+          name: 'ReubenS_FarmS',
+          short_name: 'ReubenS',
+          start_url: '/',
+          scope: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/favicon/192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable'
+            },
+            {
+              src: '/favicon/512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ]
+        },
         workbox: {
-          globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-          swDest: 'public/sw.js'
+          globPatterns: ['**/*.{js,css,html,png,svg,ico,json}'],
+          navigateFallback: '/',  // Nuxt fallback
+          navigateFallbackDenylist: [/^\/_nuxt\//, /\/api\//], // Ignore internal Nuxt files and API calls
         }
       })
     ]
