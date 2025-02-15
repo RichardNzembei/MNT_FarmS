17d1d9cf7dfc7c12ea40895ef73dfa66cfdcc16e
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-15 21:38:23 +0300

    update 3 files and delete 2 files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,44 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
- 
+import { defineNuxtConfig } from 'nuxt/config'
+import { VitePWA } from 'vite-plugin-pwa'
 
+export default defineNuxtConfig({
   modules: [
-    
     '@nuxt/ui',
-    "@pinia/nuxt"
+    '@pinia/nuxt'
   ],
+
+  vite: {
+    plugins: [
+      VitePWA({
+        manifest: {
+          name: 'ReubenS_FarmS',
+          short_name: 'ReubenS',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+            {
+              src: '/favicon/192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable'
+            },
+            {
+              src: '/favicon/512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ]
+        },
+        registerType: 'autoUpdate',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+          swDest: 'public/sw.js',
+        }
+      })
+    ]
+  },
+
+  compatibilityDate: '2025-02-15'
 })
